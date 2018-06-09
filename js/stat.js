de932357fc752f3d2ad311ed758c46cf7f31f477
EkaterinaEfimova <abramiankate@yandex.ru>
2018-06-10 02:01:45 +0300

    Правки по оформлению кода

--- js/stat.js
+++ js/stat.js
@@ -1,6 +1,6 @@
-'use strict'
+'use strict';
 
-//Создание облака с тенью и информации по игрокам
+// Создание облака с тенью и информации по игрокам
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_TEXT_X = 140;
@@ -21,61 +21,61 @@ var COLOR_MAIN_BAR = 'rgba(255, 0, 0, 1)';
 
 var barY = CLOUD_TEXT_Y + CLOUD_TEXT_GAP * 2;
 
-var renderCloud = function(ctx, x, y, color) {
-	ctx.fillStyle = color;
-	ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
 
-//нахождение максимального элемента
-var getMaxElement = function(arr) {
-	var maxElement = arr[0];
+// нахождение максимального элемента
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
 
-	for (var i = 0; i < arr.length; i++) {
-		if (arr[i] > maxElement) {
-			maxElement = arr[i];
-		}
-	}
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
 
-	return maxElement;
+  return maxElement;
 };
 
 window.renderStatisticsCloud = function (ctx) {
-//отрисовка облака
-	renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
-	renderCloud(ctx, 100, 10, '#fff');
+  // отрисовка облака
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, 100, 10, '#fff');
 
-// отрисовка заголовка
-	ctx.fillStyle = TEXT_COLOR;
-	ctx.font = TEXT_FONT;
-	ctx.fillText('Ура вы победили!', CLOUD_TEXT_X, CLOUD_TEXT_Y);
-	ctx.fillText('Список результатов:', CLOUD_TEXT_X, CLOUD_TEXT_Y + CLOUD_TEXT_GAP);
+  // отрисовка заголовка
+  ctx.fillStyle = TEXT_COLOR;
+  ctx.font = TEXT_FONT;
+  ctx.fillText('Ура вы победили!', CLOUD_TEXT_X, CLOUD_TEXT_Y);
+  ctx.fillText('Список результатов:', CLOUD_TEXT_X, CLOUD_TEXT_Y + CLOUD_TEXT_GAP);
 };
 
-window.renderStatistics = function(ctx, players, times) {
-// отрисовка облака с заголовком
-	var renderCloud = window.renderStatisticsCloud(ctx)
+window.renderStatistics = function (ctx, players, times) {
+  // отрисовка облака с заголовком
+  renderCloud = window.renderStatisticsCloud(ctx);
 
-//отрисовка гистограммы
-	var maxTime = getMaxElement(times);
+  // отрисовка гистограммы
+  var maxTime = getMaxElement(times);
 
 
-	for (var i = 0; i < players.length; i++) {
-//   MAX_BAR_HEIGHT    BAR[I]
-// ---------------- = --------
-//  BAR_HEIGHT           X
-// X = (BAR[I] * BAR_HEIGHT) / MAX_BAR_HEIGHT
+  for (var i = 0; i < players.length; i++) {
+  //   MAX_BAR_HEIGHT    BAR[I]
+  // ---------------- = --------
+  //  BAR_HEIGHT           X
+  // X = (BAR[I] * BAR_HEIGHT) / MAX_BAR_HEIGHT
 
-		var barHeight = (BAR_HEIGHT * times[i] / maxTime);
-		var gamerX = GAMER_X + (BAR_WIDTH + GAMER_GAP) * i;
+    players[i] === TITLE_GAMER_BAR ? ctx.fillStyle = COLOR_MAIN_BAR : ctx.fillStyle = 'rgba(0,0,255,' + Math.random() + ')';
 
-		players[i] == TITLE_GAMER_BAR ? ctx.fillStyle = COLOR_MAIN_BAR : ctx.fillStyle = 'rgba(0,0,255,' + Math.random() + ')';
-			
-		ctx.fillRect(gamerX, barY + (BAR_HEIGHT - barHeight) + CLOUD_TEXT_GAP + CLOUD_TEXT_GAP, BAR_WIDTH, barHeight);
-		ctx.fillStyle = TEXT_COLOR;
-		ctx.fillText(players[i], gamerX, GAMER_NAME_Y);
-		ctx.fillText(Math.round(times[i]), gamerX, barY + (BAR_HEIGHT - barHeight) + CLOUD_TEXT_GAP);
-	}
+    var barHeight = (BAR_HEIGHT * times[i] / maxTime);
+    var gamerX = GAMER_X + (BAR_WIDTH + GAMER_GAP) * i;
+
+    ctx.fillRect(gamerX, barY + (BAR_HEIGHT - barHeight) + CLOUD_TEXT_GAP + CLOUD_TEXT_GAP, BAR_WIDTH, barHeight);
+    ctx.fillStyle = TEXT_COLOR;
+    ctx.fillText(players[i], gamerX, GAMER_NAME_Y);
+    ctx.fillText(Math.round(times[i]), gamerX, barY + (BAR_HEIGHT - barHeight) + CLOUD_TEXT_GAP);
+  }
 
 };
 
