45876cf7b96022ec3c06a3cc2002190005999cc2
EkaterinaEfimova <abramiankate@yandex.ru>
2018-06-14 22:12:37 +0300

    Внесла правки в оформление

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Массивы исходных данных
-var WIZARD_NAMES = ['Иван','Хуан Себастьян','Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_LASTNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
@@ -10,7 +10,7 @@ var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 var randomElement = function (arr) {
   var random = arr[Math.floor(Math.random() * arr.length)];
   return random;
-}
+};
 
 // Показ окна настроек
 var userSetup = document.querySelector('.setup');
@@ -51,19 +51,20 @@ var wizards = [
 var renderWizards = function (wizard) {
   var wizardElement = similrWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
   return wizardElement;
-}
+};
 
 // Добавление похожих волшебников на страницу
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < wizards.length; i++) {
   fragment.appendChild(renderWizards(wizards[i]));
- }
- similarListElement.appendChild(fragment);
+}
+similarListElement.appendChild(fragment);
 
 // Показ окна с похожими магами
 userSetup.querySelector('.setup-similar').classList.remove('hidden');
+
