56390111b4112ed6ef9fa522835024ee3dde7aa6
EkaterinaEfimova <abramiankate@yandex.ru>
2018-06-16 21:02:06 +0300

    Внесла правки

--- js/setup.js
+++ js/setup.js
@@ -5,47 +5,18 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_LASTNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var NUMBER_WIZARDS = 4;
 
 // Функция генерации случайного элемента массива
 var randomElement = function (arr) {
-  var random = arr[Math.floor(Math.random() * arr.length)];
-  return random;
+  return arr[Math.floor(Math.random() * arr.length)];
 };
-
-// Показ окна настроек
-var userSetup = document.querySelector('.setup');
-userSetup.classList.remove('hidden');
 
 // Нахождение блока, в который добавляются похожие маги, и шаблона с версткой самих магов
 var similarListElement = document.querySelector('.setup-similar-list');
 var similrWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
-
-
-// Массив с сгенерированными данными для создания похожих магов
-var wizards = [
-  {
-    name: randomElement(WIZARD_NAMES) + ' ' + randomElement(WIZARD_LASTNAMES),
-    coatColor: randomElement(COAT_COLOR),
-    eyesColor: randomElement(EYES_COLOR)
-  },
-  {
-    name: randomElement(WIZARD_NAMES) + ' ' + randomElement(WIZARD_LASTNAMES),
-    coatColor: randomElement(COAT_COLOR),
-    eyesColor: randomElement(EYES_COLOR)
-  },
-  {
-    name: randomElement(WIZARD_NAMES) + ' ' + randomElement(WIZARD_LASTNAMES),
-    coatColor: randomElement(COAT_COLOR),
-    eyesColor: randomElement(EYES_COLOR)
-  },
-  {
-    name: randomElement(WIZARD_NAMES) + ' ' + randomElement(WIZARD_LASTNAMES),
-    coatColor: randomElement(COAT_COLOR),
-    eyesColor: randomElement(EYES_COLOR)
-  }
-];
 
 // Изменение шаблона волшебника
 var renderWizards = function (wizard) {
@@ -58,13 +29,21 @@ var renderWizards = function (wizard) {
   return wizardElement;
 };
 
-// Добавление похожих волшебников на страницу
+// Добавление похожих волшебников на страницу и изменение у них имени, цвета мантии и глаз
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizards(wizards[i]));
+for (var i = 0; i < NUMBER_WIZARDS; i++) {
+  var wizard = {
+    name: randomElement(WIZARD_NAMES) + ' ' + randomElement(WIZARD_LASTNAMES),
+    coatColor: randomElement(COAT_COLOR),
+    eyesColor: randomElement(EYES_COLOR)
+  };
+  fragment.appendChild(renderWizards(wizard));
 }
 similarListElement.appendChild(fragment);
 
+// Показ окна настроек
+var userSetup = document.querySelector('.setup');
+userSetup.classList.remove('hidden');
+
 // Показ окна с похожими магами
 userSetup.querySelector('.setup-similar').classList.remove('hidden');
-
